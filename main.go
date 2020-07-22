81871f9bed662f7216566e2583e142d5733f9575
chensheng06 <chensheng06@baidu.com>
2020-07-22 17:49:05 +0800

    first edition

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/baetyl/baetyl-go/v2/context"
+)
+
+func main() {
+	context.Run(func(ctx context.Context) error {
+		var cfg Config
+		err := ctx.LoadCustomConfig(&cfg)
+		if err != nil {
+			return err
+		}
+
+		pointers, err := NewPoints(cfg)
+		if err != nil {
+			return err
+		}
+
+		lines, err := NewLines(cfg, pointers)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			for _, liner := range lines {
+				liner.close()
+			}
+		}()
+
+		ctx.Wait()
+		return nil
+	})
+}
