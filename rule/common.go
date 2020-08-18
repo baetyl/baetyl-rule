e79712829fa5ffb57a61a98f8b8fe25c0e866b43
chensheng06 <chensheng06@baidu.com>
2020-08-18 20:10:43 +0800

    support load system cert

--- rule/common.go
+++ rule/common.go
@@ -8,17 +8,9 @@ import (
 	"github.com/baetyl/baetyl-rule/v2/client"
 )
 
-const (
-	BaetylRule                = "baetyl-rule"
-	BaetylEdgeSystemNamespace = "baetyl-edge-system"
-	BaetylFunction            = "baetyl-function"
-	BaetylBroker              = "baetyl-broker"
-	BaetylBrokerPort          = 1883
-)
-
 func NewClient(ruleName, refType string, ref ClientRef, clientInfo ClientInfo) (s client.Client, err error) {
 	switch clientInfo.Kind {
-	case kindMqtt:
+	case KindMqtt:
 		cfg := new(mqtt.ClientConfig)
 		err = clientInfo.Parse(cfg)
 		cfg.ClientID = generateClientID(ruleName, refType)
@@ -39,20 +31,6 @@ func NewClient(ruleName, refType string, ref ClientRef, clientInfo ClientInfo) (
 	return s, err
 }
 
-func getBrokerClient() ClientInfo {
-	return ClientInfo{
-		Name: BaetylBroker,
-		Kind: kindMqtt,
-		Value: map[string]interface{}{
-			"address": fmt.Sprintf("%s://%s:%d", "tcp", BaetylBroker, BaetylBrokerPort),
-		},
-	}
-}
-
 func generateClientID(rule, name string) string {
-	return fmt.Sprintf("%s-%s-%s", BaetylRule, rule, name)
-}
-
-func resolveFunctionAddress(function string) string {
-	return fmt.Sprintf("http://%s.%s/%s", BaetylFunction, BaetylEdgeSystemNamespace, function)
+	return fmt.Sprintf("%s-%s-%s", "baetyl-rule", rule, name)
 }
